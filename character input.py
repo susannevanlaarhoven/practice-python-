f24c91b6808cafb88779622d6e9a92ea482146c2
susannevanlaarhoven <susanne.van.laarhoven@textielmuseum.nl>
2025-04-24 22:22:30 +0200

    Add files via upload

---
+++ character input.py
@@ -0,0 +1,9 @@
+# Write your code here :-)
+name = input("Please enter your name and press ENTER: ")
+age = int(input("How old are you? "))
+year100 = (2025-age)+100
+print("Hello", name, "! Today you are", age, "years old.")
+print("In the year", year100, "you will be 100 years old.")
+num = int(input("Please enter a number: "))
+print(("In the year "+str(year100)+" you will be 100 years old.")*num)
+print(("In the year "+str(year100)+" you will be 100 years old. \n")*num)
